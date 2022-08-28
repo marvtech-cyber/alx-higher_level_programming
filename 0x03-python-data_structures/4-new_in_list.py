2a6e59c95bbe1a7ad397dfe61d34989cca6aaf44
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-08-28 04:02:07 -0700

    fixed task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    """A function tht replace an element in a copied list at a specific position."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
 
+
+def new_in_list(my_list, idx, element):
+    """
+    A function that replaces an element in a list at 
+    a specific position without modifying the original list
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
     copy = [x for x in my_list]
     copy[idx] = element
-    return (copy)
+    return copy
