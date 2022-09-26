7c3d2506f645cb4fd5c74117569281ad7f179007
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-09-26 04:26:59 -0700

    task 1 fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""My list class"""
+''' Module for MyList class '''
 
 
 class MyList(list):
-    """MyList class - a subclass of the list object"""
+    ''' Custom list class'''
 
     def print_sorted(self):
-        """Prints a sorted list"""
+        ''' Instance method:
+            print sorted list
+        '''
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
