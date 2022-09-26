b0713dc97d6aa5d66966c9f8277650b0a4eb0df1
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-09-26 05:51:03 -0700

    task 7 new logic

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Base Geometry class"""
+"""
+Contains the class BaseGeometry
+"""
 
 
 class BaseGeometry:
-    """Base Geometry Class"""
+    """A class with public instance methods area and integer_validator"""
     def area(self):
-        """calculates area"""
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        else:
-            pass
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
