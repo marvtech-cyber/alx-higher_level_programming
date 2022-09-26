9df1729bf5afb0e2bfbb37e2b2fde74c605aba7a
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-09-26 05:11:40 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module contain class BaseGeometry """
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+
+    def area(self):
+        """
+        Public instance method
+        Args:
+            self: first argument to instance methods
+        Raise:
+            Exception: area() is not implemented
+        returns: no return
+        """
+        raise Exception("area() is not implemented")
