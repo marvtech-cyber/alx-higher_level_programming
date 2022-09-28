a6dae27133b2ecd18ad71f5759643bae99bdb0f0
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-09-27 19:39:46 -0700

    task 10 redo

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+''' Module for Student class '''
 
 
 class Student:
-    """Representation of a student"""
+    ''' Representaton of a Student '''
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        ''' Instantiation '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        ''' Retrieves dictionary representation '''
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__.copy()
