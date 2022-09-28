f521e5f06f3c84daaf18a736daa00d27a88c4233
marvin nyarenchi <marvin.nyarenchi@gmail.com>
2022-09-27 19:37:06 -0700

    task 9 redo

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+''' Module for Student class '''
 
 
 class Student:
-    """Representation of a student"""
+    ''' Representaton of a Student '''
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        ''' Instantiation '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a Student instance"""
-        return self.
+        ''' Retrieves dictionary representation '''
+        return self.__dict__.copy()
