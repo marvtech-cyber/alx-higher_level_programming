37b1023663c4d44fb7a3ff3f256afd7ff71d611c
marvtech-cyber <marvin.nyarenchi@gmail.com>
2023-06-13 06:33:14 -0700

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status.
-"""
-
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
-def main():
-    """
-    Sends a GET request to https://intranet.hbtn.io/status and
-    displays the body of the response.
-    """
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
-
-
 if __name__ == "__main__":
-    main()
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
