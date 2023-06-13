6b8f9d2629bfecf2f460b1b163383e8aa17f1375
marvtech-cyber <marvin.nyarenchi@gmail.com>
2023-06-13 06:31:05 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""script that fetches https://intranet.hbtn.io/status
-using the urllib package.
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-import urllib.request
 
 
-if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(the_page)))
-    print("\t- content: {}".format(the_page))
-    print("\t- utf8 content: {}".format(the_page.decode("utf-8", 'ignore')))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
